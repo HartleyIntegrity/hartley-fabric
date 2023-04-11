af82cb430087cf5a7a5eef4490eebe1ffbd73bc3
josephizatt <joe@izatt.co.uk>
2023-04-12 00:27:03 +0100

    idk

--- api/go.mod
+++
@@ -1,7 +0,0 @@
-module main
-
-go 1.20
-
-require github.com/gorilla/mux v1.8.0
-
-require github.com/hyperledger/fabric-sdk-go v1.0.0 // indirect
--- chaincode/tenancy/tenancy.go
+++ chaincode/tenancy/tenancy.go
@@ -1,4 +1,3 @@
-// chaincode/tenancy/tenancy.go
 package main
 
 import (
@@ -12,55 +11,98 @@ type TenancyContract struct {
 	contractapi.Contract
 }
 
+type Property struct {
+	ID          string `json:"id"`
+	OwnerID     string `json:"ownerID"`
+	Address     string `json:"address"`
+	RentalPrice int    `json:"rentalPrice"`
+	Status      string `json:"status"`
+}
+
 type TenancyAgreement struct {
-	ID         string `json:"id"`
-	Landlord   string `json:"landlord"`
-	Tenant     string `json:"tenant"`
-	Property   string `json:"property"`
-	RentAmount int    `json:"rentAmount"`
-	StartDate  string `json:"startDate"`
-	EndDate    string `json:"endDate"`
+	ID               string `json:"id"`
+	PropertyID       string `json:"propertyID"`
+	TenantID         string `json:"tenantID"`
+	StartDate        string `json:"startDate"`
+	EndDate          string `json:"endDate"`
+	RentAmount       int    `json:"rentAmount"`
+	SecurityDeposit  int    `json:"securityDeposit"`
+	PaymentFrequency string `json:"paymentFrequency"`
 }
 
-func (t *TenancyContract) CreateTenancy(ctx contractapi.TransactionContextInterface, id string, landlord string, tenant string, property string, rentAmount int, startDate string, endDate string) error {
-	tenancy := TenancyAgreement{
-		ID:         id,
-		Landlord:   landlord,
-		Tenant:     tenant,
-		Property:   property,
-		RentAmount: rentAmount,
-		StartDate:  startDate,
-		EndDate:    endDate,
+// CreateProperty stores a new property on the ledger
+func (t *TenancyContract) CreateProperty(ctx contractapi.TransactionContextInterface, propertyID string, ownerID string, address string, rentalPrice int) error {
+	property := Property{
+		ID:          propertyID,
+		OwnerID:     ownerID,
+		Address:     address,
+		RentalPrice: rentalPrice,
+		Status:      "available",
 	}
 
-	tenancyBytes, err := json.Marshal(tenancy)
+	propertyJSON, err := json.Marshal(property)
 	if err != nil {
-		return fmt.Errorf("failed to marshal tenancy: %v", err)
+		return err
 	}
 
-	err = ctx.GetStub().PutState(id, tenancyBytes)
-	if err != nil {
-		return fmt.Errorf("failed to put tenancy to state: %v", err)
-	}
-
-	return nil
+	return ctx.GetStub().PutState(propertyID, propertyJSON)
 }
 
-func (t *TenancyContract) GetTenancy(ctx contractapi.TransactionContextInterface, id string) (*TenancyAgreement, error) {
-	tenancyBytes, err := ctx.GetStub().GetState(id)
+// GetProperty returns a property by its ID
+func (t *TenancyContract) GetProperty(ctx contractapi.TransactionContextInterface, propertyID string) (*Property, error) {
+	propertyJSON, err := ctx.GetStub().GetState(propertyID)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get tenancy from state: %v", err)
+		return nil, fmt.Errorf("failed to read property: %v", err)
+	}
+	if propertyJSON == nil {
+		return nil, fmt.Errorf("property not found: %s", propertyID)
 	}
 
-	if tenancyBytes == nil {
-		return nil, fmt.Errorf("tenancy not found: %s", id)
-	}
-
-	var tenancy TenancyAgreement
-	err = json.Unmarshal(tenancyBytes, &tenancy)
+	var property Property
+	err = json.Unmarshal(propertyJSON, &property)
 	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal tenancy: %v", err)
+		return nil, err
 	}
 
-	return &tenancy, nil
+	return &property, nil
+}
+
+// CreateTenancyAgreement stores a new tenancy agreement on the ledger
+func (t *TenancyContract) CreateTenancyAgreement(ctx contractapi.TransactionContextInterface, agreementID string, propertyID string, tenantID string, startDate string, endDate string, rentAmount int, securityDeposit int, paymentFrequency string) error {
+	agreement := TenancyAgreement{
+		ID:               agreementID,
+		PropertyID:       propertyID,
+		TenantID:         tenantID,
+		StartDate:        startDate,
+		EndDate:          endDate,
+		RentAmount:       rentAmount,
+		SecurityDeposit:  securityDeposit,
+		PaymentFrequency: paymentFrequency,
+	}
+
+	agreementJSON, err := json.Marshal(agreement)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(agreementID, agreementJSON)
+}
+
+// GetTenancyAgreement returns a tenancy agreement by its ID
+func (t *TenancyContract) GetTenancyAgreement(ctx contractapi.TransactionContextInterface, agreementID string) (*TenancyAgreement, error) {
+	agreementJSON, err := ctx.GetStub().GetState(agreementID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read tenancy agreement: %v", err)
+	}
+	if agreementJSON == nil {
+		return nil, fmt.Errorf("tenancy agreement not found: %s", agreementID)
+	}
+
+	var agreement TenancyAgreement
+	err = json.Unmarshal(agreementJSON, &agreement)
+	if err != nil {
+		return nil, err
+	}
+
+	return &agreement, nil
 }
